f90d1267da4c44d3cdd2476963daeca0e98b4d9d
espinorax <espinozandres@gmail.com>
2017-01-03 09:27:23 -0400

    Add files via upload

---
+++ MiniReto.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for num in 0...100 {
+
+    if num % 5 == 0 {
+        print(" \(num) Bingo!!!")
+    }
+    else if num % 2 == 0 {
+        print(" \(num) Par!!!")
+    }
+    else {
+        print(" \(num) Impar!!!")
+    }
+    if num >= 30 && num <= 40 {
+        print(" \(num) Viva Swift!!!!")
+    }
+}
+
